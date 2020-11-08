9b4c6463637d4a947e28c4e00c15bdf7a9d73317
Aritra Bhattacharjee <analyzeninvest@prtonmail.com>
2020-11-09 01:20:09 +0530

    added comparison excel func

--- main.py
+++ main.py
@@ -8,8 +8,40 @@ def search_for_excel(ticker):
         if re.match(xlsx_name, files):
             return(path + xlsx_name)
         
-def make_industry_comparison_excel(list_of_stocks, dict_of_attribute):
-    pass
+def make_comparison_excel(comparison_name, list_of_stocks, dict_of_attribute):
+    import pandas as pd
+    from datetime import date
+    from openpyxl import Workbook
+    path = "/home/arnashree/analyzeninvest-projects/analysis-and-valuation/comparison_path/"
+    excel_name = path + comparison_name + ".xlsx"
+    wb = Workbook()
+    wb.save(excel_name)
+    today = date.today()
+    current_year = today.year
+    array_of_stocks_with_attributes = fetch_attributes_from_excel(list_of_stocks, dict_of_attribute)
+    start_index = 1
+    writer = pd.ExcelWriter(excel_name, engine = 'openpyxl')
+    for stock in list_of_stocks:
+        print("################")
+        print("Starting for " + stock)
+        print("################")
+        index_array = []
+        for i in range(0,19):
+            index_array.append(current_year - i)
+        index = {"Year": index_array}
+        dict_stock_attribute = index
+        for items in array_of_stocks_with_attributes:
+            for key in items:
+                if key == stock:
+                    dict_stock_attribute.update(items[key])
+                    start_index += 1 
+        #print(dict_stock_attribute)
+        df_stock = pd.DataFrame(data = dict_stock_attribute)
+        df_stock.set_index("Year")
+        df_stock_T = df_stock.T
+        df_stock_T.to_excel(writer, sheet_name = stock)
+        writer.save()
+        #print(df_stock_T)
 
 def fetch_attributes_from_excel(list_of_stocks, dict_of_attribute):
     """
@@ -19,7 +51,7 @@ def fetch_attributes_from_excel(list_of_stocks, dict_of_attribute):
     stock_sheet_attribute_details = []
     for stock in list_of_stocks:
         print("################")
-        print("Starting for " + stock)
+        print("Fetching for " + stock)
         print("################")
         stock_xls_path = search_for_excel(stock)
         if not stock_xls_path:
@@ -74,7 +106,8 @@ def main():
                              "Total Revenue",
                              "Depreciation And Amortisation Expenses"
                          ]}
-    print(fetch_attributes_from_excel(stock_ticker_list, dict_of_attribute))
+    #print(fetch_attributes_from_excel(stock_ticker_list, dict_of_attribute))
+    make_comparison_excel("Defence",stock_ticker_list, dict_of_attribute)
 
 if __name__ == '__main__':
     main()
