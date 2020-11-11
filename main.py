068c0fb4b8d14be11e4bf0682dccd8922c4b8488
Aritra Bhattacharjee <analyzeninvest@prtonmail.com>
2020-11-11 22:36:42 +0530

    working all sheets

--- main.py
+++ main.py
@@ -8,19 +8,50 @@ def search_for_excel(ticker):
         if re.match(xlsx_name, files):
             return(path + xlsx_name)
         
-def make_comparison_excel(comparison_name, list_of_stocks, dict_of_attribute):
+
+def make_info_page_of_comparison_excel(excel_path, list_of_stocks, dict_of_attribute):
     import pandas as pd
-    from datetime import date
+    #########################
+    # # make the info sheet #
+    #########################
+    # name of stocks
+    writer = pd.ExcelWriter(excel_path, engine = 'openpyxl')
+    df_info_stocks = pd.DataFrame(data={"Stocks":list_of_stocks})
+    df_info_stocks.to_excel(writer, sheet_name = "info", startcol=0, index=False)
+    print(df_info_stocks)
+    # name of parameters
+    all_attributes = []
+    for key in dict_of_attribute:
+        all_attributes += dict_of_attribute[key]
+    df_info_attributes = pd.DataFrame(data={"Parameters": all_attributes})
+    df_info_attributes.to_excel(writer, sheet_name = "info", startcol=1, index=False)
+    print(df_info_attributes)
+    #writer.save()
+    #writer.close()
+
+def make_chart_page_of_comparison_excel(excel_path, list_of_stocks, dict_of_attribute):
     from openpyxl import Workbook
     from openpyxl.worksheet.datavalidation import DataValidation
-    path = "/home/arnashree/analyzeninvest-projects/analysis-and-valuation/comparison_path/"
-    excel_name = path + comparison_name + ".xlsx"
-    writer = pd.ExcelWriter(excel_name, engine = 'openpyxl')
+    ################################
+    # # make the chart excel sheet #
+    ################################
     wb = Workbook()
-    wb.save(excel_name)
+    ws = wb.create_sheet("Chart")
+    cell = ws['A1']
+    cell.value = "Parameter"
+    data_val = DataValidation(type="list",formula1='=$info$B:$info$B')
+    ws.add_data_validation(data_val)
+    data_val.add(ws["A2"])
+    #wb.save(excel_path)
+    #wb.close()
+
+def make_all_stocks_pages_comparison_excel(excel_path, list_of_stocks, dict_of_attribute):
+    import pandas as pd
+    from datetime import date
     ########################################
     # make other Stock details excel sheet #
     ########################################
+    writer = pd.ExcelWriter(excel_path, engine = 'openpyxl')
     today = date.today()
     current_year = today.year
     array_of_stocks_with_attributes = fetch_attributes_from_excel(list_of_stocks, dict_of_attribute)
@@ -42,16 +73,28 @@ def make_comparison_excel(comparison_name, list_of_stocks, dict_of_attribute):
         df_stock.set_index("Year")
         df_stock_T = df_stock.T
         df_stock_T.to_excel(writer, sheet_name = stock)
-        writer.save()
+        #writer.save()
+    #writer.close()
         #print(df_stock_T)
-    #########################
-    # # make the info sheet #
-    #########################
-    # name of stocks
+
+    
+def make_comparison_excel(comparison_name, list_of_stocks, dict_of_attribute):
+    import pandas as pd
+    from datetime import date
+    from openpyxl import Workbook
+    from openpyxl.worksheet.datavalidation import DataValidation
+    from openpyxl import load_workbook
+    path = "/home/arnashree/analyzeninvest-projects/analysis-and-valuation/comparison_path/"
+    excel_path = path + comparison_name + ".xlsx"
+    wb = Workbook()
+    wb.save(excel_path)
+    ######################
+    # make the info page #
+    ######################
+    writer = pd.ExcelWriter(excel_path, engine = 'openpyxl')
     df_info_stocks = pd.DataFrame(data={"Stocks":list_of_stocks})
     df_info_stocks.to_excel(writer, sheet_name = "info", startcol=0, index=False)
     print(df_info_stocks)
-    writer.save()
     # name of parameters
     all_attributes = []
     for key in dict_of_attribute:
@@ -59,18 +102,43 @@ def make_comparison_excel(comparison_name, list_of_stocks, dict_of_attribute):
     df_info_attributes = pd.DataFrame(data={"Parameters": all_attributes})
     df_info_attributes.to_excel(writer, sheet_name = "info", startcol=1, index=False)
     print(df_info_attributes)
+    ################################
+    # make the page for all stocks #
+    ################################
+    #writer = pd.ExcelWriter(excel_path, engine = 'openpyxl')
+    today = date.today()
+    current_year = today.year
+    array_of_stocks_with_attributes = fetch_attributes_from_excel(list_of_stocks, dict_of_attribute)
+    for stock in list_of_stocks:
+        print("################")
+        print("Starting for " + stock)
+        print("################")
+        index_array = []
+        for i in range(0,19):
+            index_array.append(current_year - i)
+        index = {"Year": index_array}
+        dict_stock_attribute = index
+        for items in array_of_stocks_with_attributes:
+            for key in items:
+                if key == stock:
+                    dict_stock_attribute.update(items[key])
+        #print(dict_stock_attribute)
+        df_stock = pd.DataFrame(data = dict_stock_attribute)
+        df_stock.set_index("Year")
+        df_stock_T = df_stock.T
+        df_stock_T.to_excel(writer, sheet_name = stock)
     writer.save()
-    ################################
-    # # make the chart excel sheet #
-    ################################
-    wb = Workbook()
+    #######################
+    # make the chart page #
+    #######################
+    wb = load_workbook(excel_path)
     ws = wb.create_sheet("Chart")
     cell = ws['A1']
     cell.value = "Parameter"
     data_val = DataValidation(type="list",formula1='=$info$B:$info$B')
     ws.add_data_validation(data_val)
     data_val.add(ws["A2"])
-    wb.save(excel_name)
+    wb.save(excel_path)
 
         
 
