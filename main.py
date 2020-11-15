e2d6e6e2542d838f9f01a28dc2b563314a748f66
Aritra Bhattacharjee <analyzeninvest@prtonmail.com>
2020-11-15 09:39:03 +0530

    modified data format, printing formula as a string

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def make_comparison_excel(comparison_name, list_of_stocks, dict_of_attribute):
     from openpyxl import Workbook
     from openpyxl.worksheet.datavalidation import DataValidation
     from openpyxl import load_workbook
+    import string
     path = "/home/arnashree/analyzeninvest-projects/analysis-and-valuation/comparison_path/"
     excel_path = path + comparison_name + ".xlsx"
     wb = Workbook()
@@ -25,14 +26,14 @@ def make_comparison_excel(comparison_name, list_of_stocks, dict_of_attribute):
     writer = pd.ExcelWriter(excel_path, engine = 'openpyxl')
     df_info_stocks = pd.DataFrame(data={"Stocks":list_of_stocks})
     df_info_stocks.to_excel(writer, sheet_name = "info", startcol=0, index=False)
-    print(df_info_stocks)
+    #print(df_info_stocks)
     # name of parameters
     all_attributes = []
     for key in dict_of_attribute:
         all_attributes += dict_of_attribute[key]
     df_info_attributes = pd.DataFrame(data={"Parameters": all_attributes})
     df_info_attributes.to_excel(writer, sheet_name = "info", startcol=1, index=False)
-    print(df_info_attributes)
+    #print(df_info_attributes)
     ################################
     # make the page for all stocks #
     ################################
@@ -44,14 +45,16 @@ def make_comparison_excel(comparison_name, list_of_stocks, dict_of_attribute):
         print("################")
         print("Starting for " + stock)
         print("################")
-        index_array = []
+        years_array = []
+        uniqe_items_list = []
         for i in range(0,19):
-            index_array.append(current_year - i)
-        index = {"Year": index_array}
+            years_array.append(current_year - i)
+        index = {"Year": years_array}
         dict_stock_attribute = index
         for items in array_of_stocks_with_attributes:
             for key in items:
                 if key == stock:
+                    uniqe_items_list.append(key)
                     dict_stock_attribute.update(items[key])
         #print(dict_stock_attribute)
         df_stock = pd.DataFrame(data = dict_stock_attribute)
@@ -62,33 +65,52 @@ def make_comparison_excel(comparison_name, list_of_stocks, dict_of_attribute):
     #######################
     # make the chart page #
     #######################
-    wb = load_workbook(excel_path)
-    ws = wb.create_sheet("Chart")
-    cell = ws['A1']
-    # data validation for all the parameters
-    cell.value = "Parameter"
-    #
-    #############################
-    # from here its not working #
-    #############################
-    formula = '=$info.$B$2:$B$'+str(len(all_attributes)-1)
-    #print(formula)
-    data_val = DataValidation(type="list",formula1=formula)
-    ws.add_data_validation(data_val)
-    data_val.add(ws["A2"])
-    #########################################################################
-    # ERROR: data val does not work!! the formula is putting in lowercase ! #
-    #########################################################################
     # all stock names
-    cell = ws['B1']
-    cell.value = "Stock Name"
-    for i in range(0,len(list_of_stocks)):
-        cell_target = 'B' + str(2 + i)
-        cell = ws[cell_target]
-        cell.value = list_of_stocks[i]
-    wb.save(excel_path)
+    # cell = ws['B1']
+    # cell.value = "Stock Name"
+    # for i in range(0,len(list_of_stocks)):
+    #     cell_target = 'B' + str(2 + i)
+    #     cell = ws[cell_target]
+    #     cell.value = list_of_stocks[i]
+    # wb.save(excel_path)
     # years
+    dict_stock_formula = {"Year":years_array}
+    #print(len(years_array))
     # construct index match formula
+    # sample :
+    # =INDEX($BDL.B2:B3,MATCH(A2,$info.B2:B3))
+    # =INDEX($BDL.C2:C3,MATCH(A2,$info.B2:B3))
+    # =INDEX($ASTRAMICRO.B2:B3,MATCH(A2,$info.B2:B3))
+    # =INDEX($ASTRAMICRO.C2:C3,MATCH(A2,$info.B2:B3))
+    for stock in list_of_stocks:
+        array_of_stock_formula = []
+        for char in string.ascii_uppercase:
+            if char not in ['A', 'U', 'V', 'W', 'X', 'Y', 'Z']:
+                formula = " = INDEX($" + stock + ".$" + char + "$3:$" + char + "$" + str(len(uniqe_items_list) +2) + ",MATCH(A2, $info.$B$2:$B$" + str(len(uniqe_items_list) +1) + ",0)" + ")"
+                #print(formula)
+                array_of_stock_formula.append(formula)
+        #print(len(array_of_stock_formula))
+        dict_stock_formula.update({stock:array_of_stock_formula})
+    #print(dict_stock_formula)
+    df_stock_formula = pd.DataFrame(data = dict_stock_formula)
+    df_stock_formula.set_index("Year")
+    df_stock_formula_T = df_stock_formula.T
+    df_stock_formula_T.to_excel(writer, sheet_name = "Chart", startcol=2)
+    writer.save()
+    wb = load_workbook(excel_path)
+    #print(wb)
+    for ws in wb:
+        if ws.title == "Chart":
+            cell = ws['A1']
+            # data validation for all the parameters
+            cell.value = "Parameter"
+            formula = '=$info.$B$2:$B$'+str(len(uniqe_items_list)+1)
+            #print(formula)
+            data_val = DataValidation(type="list",formula1=formula)
+            ws.add_data_validation(data_val)
+            data_val.add(ws["A2"])
+    wb.save(excel_path)
+    # make chart
     # make the chart
 
         
@@ -130,7 +152,13 @@ def fetch_attributes_from_excel(list_of_stocks, dict_of_attribute):
                                     value = []
                                     key = sheet[column + str(2)].value
                                     for index in range(3, 22):
-                                        value.append(sheet[column + str(index)].value)
+                                        raw_value = sheet[column + str(index)].value
+                                        if raw_value == "--":
+                                            raw_value = 0
+                                        else:
+                                            raw_value = raw_value.replace(",", "")
+                                            raw_value = float(raw_value)
+                                        value.append(raw_value) # modify here for value change in format
                                     key_value_pair = {key:value}
                                     stock_sheet_attribute_details.append({stock: key_value_pair})
                                     #print(key_value_pair)
